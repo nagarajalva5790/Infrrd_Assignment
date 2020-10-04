8c8cfb098d5f26e4ee26f6a8d111c14c487116c5
nagarajalva5790 <nagarajalva5790@gmail.com>
2020-10-04 14:24:56 +0300

    Updates

---
+++ precache-manifest.c23f92ea1b0ef4557f209833105f1e46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fff015ceb93ce66a403db76cd1f7f4e7",
+    "url": "/Infrrd_Assignment/index.html"
+  },
+  {
+    "revision": "6a5ceb4e32bf9079877e",
+    "url": "/Infrrd_Assignment/static/css/main.d239d82f.chunk.css"
+  },
+  {
+    "revision": "be2d6ec410888acb71c7",
+    "url": "/Infrrd_Assignment/static/js/2.fa866f76.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/Infrrd_Assignment/static/js/2.fa866f76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a5ceb4e32bf9079877e",
+    "url": "/Infrrd_Assignment/static/js/main.f8867f9e.chunk.js"
+  },
+  {
+    "revision": "8f6c323979c270308fa2",
+    "url": "/Infrrd_Assignment/static/js/runtime-main.d18abc63.js"
+  }
+]);
